c29e6901ce363987d2c007ea419c22b6a533e4d4
Craig Gent <genty83@outlook.com>
2024-11-02 17:12:13 +0000

    Added forms

---
+++ products/forms.py
@@ -0,0 +1,56 @@
+"""
+Forms for managing products in the application.
+"""
+
+from django import forms
+from .models import Product, Category, SubCategory
+
+# Add product form
+class ProductForm(forms.ModelForm):
+    """
+    Form for adding a new product.
+    """
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize the form with category and subcategory choices.
+        """
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        sub_categories = SubCategory.objects.all()
+        category_ids = [(int(c.id), c.get_friendly_name()) for c in categories]
+        subcategory_ids = [(int(s.id), s.get_friendly_name()) for s in sub_categories]
+
+        self.fields['category_id'].choices = category_ids
+        self.fields['subcategory_id'].choices = subcategory_ids
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
+
+
+# Update product form
+class UpdateProductForm(forms.ModelForm):
+    """
+    Form for updating an existing product.
+    """
+    class Meta:
+        model = Product
+        fields = '__all__'
+        exclude = ['slug', 'date_added']
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize the form with category and subcategory choices.
+        """
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        sub_categories = SubCategory.objects.all()
+        category_ids = [(int(c.id), c.get_friendly_name()) for c in categories]
+        subcategory_ids = [(int(s.id), s.get_friendly_name()) for s in sub_categories]
+
+        self.fields['category_id'].choices = category_ids
+        self.fields['subcategory_id'].choices = subcategory_ids
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
