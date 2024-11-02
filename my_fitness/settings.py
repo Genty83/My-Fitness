0d5c9b71d42a0e10cbef6a4b18eca3bcb885daf6
Craig Gent <genty83@outlook.com>
2024-11-02 12:26:41 +0000

    Updated settings

--- my_fitness/settings.py
+++ my_fitness/settings.py
@@ -13,12 +13,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "sfgsghe"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["8000-genty83-myfitness-9gh4jnl8ilo.ws.codeinstitute-ide.net"]
+ALLOWED_HOSTS = ["8000-genty83-myfitness-y0n06gwpb2f.ws.codeinstitute-ide.net"]
 
 
 # Application definition
