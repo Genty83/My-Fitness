a62921b426f930737e92894fe1af2ceb7a026795
Craig Gent <genty83@outlook.com>
2024-11-02 19:46:55 +0000

    Small changes to function

--- static/js/main.js
+++ static/js/main.js
@@ -2,6 +2,17 @@
  * @fileoverview This is the main entry file to the js used for this project
  */
 
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Sidebar
+  eventHandler('click', '.menu-btn', () => {
+    toggleSideMenu();
+  });
+
+  // Get the class names from the config file
+  formatElements();
+});
+
 const eventHandler = (type, selectpr, callback, options) => {
   document.addEventListener(type, (e) => {
     if (e.target.matches(selectpr)) {
@@ -10,25 +21,12 @@ const eventHandler = (type, selectpr, callback, options) => {
   }, options);
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  // Sidebar
-  eventHandler('click', '.menu-btn', () => {
-    const sidebar = document.querySelector('.mobile-nav');
+const toggleSideMenu = () => {
+  const sidebar = document.querySelector('.mobile-nav');
 
     if (sidebar.getBoundingClientRect().width === 0) {
       sidebar.style.width = '280px';
     } else {
       sidebar.style.width = '0px';
     }
-  });
-});
-
-// Function to toggle side menu
-function toggleMenu() {
-  const sideMenu = document.querySelector('.mobile-nav');
-  if (sideMenu.getBoundingClientRect().width == 0) {
-    sideMenu.style.width = '300px';
-  } else {
-    sideMenu.style.width = '0px';
-  }
 }
