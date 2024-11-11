197013e8fd0b82171156044bea900f70d4be4b84
Craig Gent <genty83@outlook.com>
2024-11-11 16:43:20 +0000

    Updated home view

--- home/views.py
+++ home/views.py
@@ -4,6 +4,9 @@ from newsletter.models import SubscribeToNewsletter
 # Create your views here.
 def home(request):
 
+    # Check if the signed in user is subscribed to the newsletter
+    subscribed_user = False
+
     if request.user.is_authenticated:
         subscribed_user = SubscribeToNewsletter.objects.filter(
             email=request.user.email).exists()
