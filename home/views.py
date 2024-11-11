76a0d483d1e8204645c1493d511a0ca7041795e8
Craig Gent <genty83@outlook.com>
2024-11-11 16:24:01 +0000

    Updated home view and base template

--- home/views.py
+++ home/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from newsletter.models import SubscribeToNewsletter
 
 # Create your views here.
 def home(request):
 
-    return render(request, 'home/home.html')
+    if request.user.is_authenticated:
+        subscribed_user = SubscribeToNewsletter.objects.filter(
+            email=request.user.email).exists()
+
+    context = {
+        'subscribed_user': subscribed_user
+    }
+
+    return render(request, 'home/home.html', context)
