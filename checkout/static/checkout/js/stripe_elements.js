81ae9b57a9e64154edea0872e7c08b063b21c620
Craig Gent <genty83@outlook.com>
2024-11-14 14:09:13 +0000

    Added stripe functionality

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,62 @@
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
+var elements = stripe.elements();
+
+var style = {
+  base: {
+    color: '#f1f5f9'
+  },
+  invalid: {
+    color: '#f87171'
+  }
+};
+var card = elements.create('card', {style: style});
+card.mount('#card-element')
+
+// Handle errors
+card.addEventListener('change', (e) => {
+  var errorDiv = document.getElementById('card-errors');
+  if (e.error) {
+    var html = `
+    <span role="alert">
+      <i class="fas fa-times ft-red-400"></i>
+    </span>
+    <span class="ft-red-400">${e.error.message}</span>
+    `;
+    $(errorDiv).html(html);
+  }
+  else {
+    errorDiv.textContent = '';
+  }
+});
+
+// Handle form submit 
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  card.update({ 'disabled': true });
+  $('#submit-button').attr('disabled', true);
+  stripe.confirmCardPayment(clientSecret, {
+    payment_method: {
+      card: card,
+    }
+  }).then((result) => {
+    if (result.error) {
+      var html = `
+        <span role="alert">
+          <i class="fas fa-times ft-red-400"></i>
+        </span>
+        <span class="ft-red-400">${result.error.message}</span>
+      `;
+      $(errorDiv).html(html);
+      card.update({ 'disabled': false });
+      $('#submit-button').attr('disabled', false);
+    } else {
+      if (result.paymentIntent.status === 'succeeded') {
+        form.submit();
+      }
+    }
+  })
+});
