a269cff8f2baafa206c8bf7d91d4e74fc381980d
Craig Gent <genty83@outlook.com>
2024-11-16 10:40:05 +0000

    Made changes in view, form models and signals

--- checkout/forms.py
+++ checkout/forms.py
@@ -5,9 +5,16 @@ from .models import Order
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ('full_name', 'email', 'phone_number', 'street_address1',
-                    'street_address2', 'town_or_city', 'postcode',
-                    'country', 'county')
+        fields = (
+            'full_name', 
+            'email', 
+            'phone_number',
+            'postcode',
+            'town_or_city', 
+            'street_address1',
+            'street_address2',
+            'country', 
+            'county')
         
     def __init__(self, *args, **kwargs):
         """
--- checkout/views.py
+++ checkout/views.py
@@ -13,37 +13,47 @@ def checkout(request):
 
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
-    intent = None
 
     if request.method == 'POST':
         bag = request.session.get('bag', {})
+
         form_data = {
-            'full_name': request.POST.get('full_name', ''),
-            'email': request.POST.get('email', ''),
-            'phone_number': request.POST.get('phone_number', ''),
-            'street_address1': request.POST.get('street_address1', ''),
-            'street_address2': request.POST.get('street_address2', ''),
-            'town_or_city': request.POST.get('town_or_city', ''),
-            'postcode': request.POST.get('postcode', ''),
-            'country': request.POST.get('country', ''),
-            'county': request.POST.get('county', ''),
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
         }
+
         order_form = OrderForm(form_data)
         if order_form.is_valid():
             order = order_form.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
-                    order_line_item = OrderLineItem(
-                        order=order,
-                        product=product,
-                        quantity=item_data,
-                    )
-                    order_line_item.save()
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            product=product,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                    else:
+                        for size, quantity in item_data['items_by_size'].items():
+                            order_line_item = OrderLineItem(
+                            order=order,
+                            product=product,
+                            quantity=quantity,
+                            product_size=size,
+                        )
                 except Product.DoesNotExist:
                     messages.error(request, (
-                        "One of the products in your bag wasn't found in our database."
-                        "Please call us for assistance!")
+                        "One of the produccts in your bag wasn't fount in our database. "
+                        "Please call for assistance!")
                     )
                     order.delete()
                     return redirect(reverse('bag'))
@@ -68,11 +78,14 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY
         )
 
-        if not stripe_public_key:
-            messages.warning('Stripe public key is missing. \
-                Did you forget to set it in your enviroment?')
+
+
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing.')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
