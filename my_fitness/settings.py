cbed9ac8b791b39709d46bc7b2b5c7ef3c5f2f37
Craig Gent <genty83@outlook.com>
2024-11-19 13:54:03 +0000

    Updated settings

--- my_fitness/settings.py
+++ my_fitness/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEVELOPMENT" in os.environ
 
-ALLOWED_HOSTS = ["local-host"]
+ALLOWED_HOSTS = ["https://my-fitness-7a85d9b108b0.herokuapp.com/", "local-host"]
 CSRF_TRUSTED_ORIGINS = ['https://8000-genty83-myfitness-y0n06gwpb2f.ws.codeinstitute-ide.net']
 
 
@@ -173,7 +173,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'myfitnessstore'
-    AWS_S3_REGION_NAME = 'eu-north-1'
+    AWS_S3_REGION_NAME = 'Europe (Stockholm) eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', '')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
