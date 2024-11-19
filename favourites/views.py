7bb2db0fc67f40d609e7282652c24a3738b3092b
Craig Gent <genty83@outlook.com>
2024-11-19 13:29:30 +0000

    Deleted app

--- favourites/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def favourites(request):
-
-    context = {
-        
-    }   
-    return render(request, 'favourites/favourites.html', context)
