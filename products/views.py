ecd1835d7767c7b4bd2786c254d4caa8433ced20
Craig Gent <genty83@outlook.com>
2024-11-21 19:58:26 +0000

    Cleaned up code

--- products/views.py
+++ products/views.py
@@ -1,12 +1,12 @@
 from django.contrib import messages
 from django.shortcuts import redirect, reverse, render
+from django.template.defaulttags import register
+from django.contrib.auth.decorators import login_required
 from .models import Product, Category, SubCategory
 from .product_utils import (
     search_products, sort_products, filter_products, paginate_products,
     get_items_per_category, get_items_per_subcategory
-    )
-from django.template.defaulttags import register
-from django.contrib.auth.decorators import login_required
+)
 from .forms import ProductForm
 from reviews.models import ProductReview
 
@@ -20,17 +20,6 @@ def get_item(dictionary, key):
 def all_products(request):
     """
     View to display all products with optional search, sort, and filter functionality.
-    
-    Retrieves all sub-categories and products. Applies search, sort, and filter 
-    operations based on GET parameters. Paginates the products list based on 
-    the 'items_per_page' parameter from the GET request.
-
-    Args:
-        request (HttpRequest): The HTTP request object.
-
-    Returns:
-        HttpResponse: The rendered 'all_products.html' template with the context 
-        containing products, categories, sub-categories, and items per category/subcategory.
     """
     products = Product.objects.all()
     categories = Category.objects.all()
@@ -42,25 +31,22 @@ def all_products(request):
         products = filter_products(request, products)
 
     items_per_page = 6
-    # Update items per page if user selects a different value
     if request.GET.get('items_per_page'):
         items_per_page = request.GET.get('items_per_page')
 
     items_per_category = get_items_per_category(products)
     items_per_subcategory = get_items_per_subcategory(products)
-
     products = paginate_products(request, products, items_per_page)
 
-    # Get avg rating using the get_average_rating function
     for product in products:
         product.rating = ProductReview.get_average_rating(product)
 
     context = {
-        'products':  products,
+        'products': products,
         'categories': categories,
         'sub_categories': sub_categories,
         'items_per_category': items_per_category,
-        'items_per_subcategory': items_per_subcategory
+        'items_per_subcategory': items_per_subcategory,
     }
 
     return render(request, 'products/all_products.html', context)
@@ -69,57 +55,32 @@ def all_products(request):
 def product_detail(request, product_id):
     """
     View to display the details of a single product.
-    
-    Retrieves the product based on the provided product_id and renders the 
-    'product_detail.html' template with the product context.
-
-    Args:
-        request (HttpRequest): The HTTP request object.
-        product_id (int): The ID of the product to be retrieved.
-
-    Returns:
-        HttpResponse: The rendered 'product_detail.html' template with the context 
-        containing the product.
     """
     product = Product.objects.get(pk=product_id)
     reviews = ProductReview.objects.filter(product=product)
-    # Calculate the stars and half stars for each review
     for review in reviews:
         review.stars = range(int(review.rating))
-        if review.rating % 1 != 0:
-            review.half_star = True
+        review.half_star = (review.rating % 1 != 0)
 
     related_products = Product.objects.filter(
         category_id=product.category_id).exclude(id=product.id).order_by('?')[:4]
-    
-    template = 'products/product_detail.html'
+
     context = {
         'product': product,
         'reviews': reviews,
         'categories': Category.objects.all(),
         'sub_categories': SubCategory.objects.all(),
         'related_products': related_products,
-
     }
-    return render(request, template, context)
+
+    return render(request, 'products/product_detail.html', context)
 
 
 @login_required
 def add_product(request):
     """
     View to add a new product to the database.
-    
-    Renders the 'add_product.html' template with the form to add a new product.
-
-    Args:
-        request (HttpRequest): The HTTP request object.
-
-    Returns:
-        HttpResponse: The rendered 'add_product.html' template.
     """
-
-    # If the form is submitted save the product to the database
-    # and redirect to the product detail page showing a success message
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
@@ -131,36 +92,20 @@ def add_product(request):
     else:
         form = ProductForm()
 
-
     context = {
         'categories': Category.objects.all(),
         'sub_categories': SubCategory.objects.all(),
         'form': form,
     }
 
-    template = 'products/add_product.html'
-    return render(request, template, context)
+    return render(request, 'products/add_product.html', context)
 
 
 @login_required
 def edit_product(request, product_id):
     """
     View to edit an existing product in the database.
-    
-    Retrieves the product based on the provided product_id and renders the 
-    'edit_product.html' template with the product context.
-
-    Args:
-        request (HttpRequest): The HTTP request object.
-        product_id (int): The ID of the product to be retrieved.
-
-    Returns:
-        HttpResponse: The rendered 'edit_product.html' template with the context 
-        containing the product.
     """
-
-    # If the form is submitted save the product to the database
-    # and redirect to the product detail page showing a success message
     product = Product.objects.get(pk=product_id)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
@@ -173,7 +118,7 @@ def edit_product(request, product_id):
     else:
         form = ProductForm(instance=product)
         messages.info(request, f'You are editing {product.name}')
-    
+
     context = {
         'form': form,
         'product': product,
@@ -181,30 +126,14 @@ def edit_product(request, product_id):
         'sub_categories': SubCategory.objects.all(),
     }
 
-    template = 'products/edit_product.html'
-
-    return render(request, template, context)
+    return render(request, 'products/edit_product.html', context)
 
 
 @login_required
 def delete_product(request, product_id):
     """
     View to delete an existing product from the database.
-    
-    Retrieves the product based on the provided product_id and renders the 
-    'delete_product.html' template with the product context.
-
-    Args:
-        request (HttpRequest): The HTTP request object.
-        product_id (int): The ID of the product to be retrieved.
-
-    Returns:
-        HttpResponse: The rendered 'delete_product.html' template with the context 
-        containing the product.
     """
-
-    # Add a confirmation message before deleting the product
-    # and redirect to the all products page showing a success message
     if request.method == 'POST':
         product = Product.objects.get(pk=product_id)
         product.delete()
@@ -218,16 +147,6 @@ def delete_product(request, product_id):
 def sale_products(request):
     """
     View to display all products on sale.
-    
-    Retrieves all products that have a discount applied and renders the 
-    'sale_products.html' template with the products context.
-
-    Args:
-        request (HttpRequest): The HTTP request object.
-
-    Returns:
-        HttpResponse: The rendered 'sale_products.html' template with the context 
-        containing products.
     """
     products = Product.objects.filter(discount_price__isnull=False)
     categories = Category.objects.all()
@@ -239,25 +158,22 @@ def sale_products(request):
         products = filter_products(request, products)
 
     items_per_page = 6
-    # Update items per page if user selects a different value
     if request.GET.get('items_per_page'):
         items_per_page = request.GET.get('items_per_page')
 
     items_per_category = get_items_per_category(products)
     items_per_subcategory = get_items_per_subcategory(products)
-
     products = paginate_products(request, products, items_per_page)
 
-    # Get avg rating using the get_average_rating function
     for product in products:
         product.rating = ProductReview.get_average_rating(product)
 
     context = {
-        'products':  products,
+        'products': products,
         'categories': categories,
         'sub_categories': sub_categories,
         'items_per_category': items_per_category,
-        'items_per_subcategory': items_per_subcategory
+        'items_per_subcategory': items_per_subcategory,
     }
 
-    return render(request, 'products/all_products.html', context)
+    return render(request, 'products/sale_products.html', context)
