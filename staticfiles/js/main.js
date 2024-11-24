d3c36d1a518c2e923d5bad5f0a2524e8e4615e01
Craig Gent <genty83@outlook.com>
2024-11-24 14:48:31 +0000

    Updated static

--- staticfiles/js/main.js
+++ staticfiles/js/main.js
@@ -1,38 +1,64 @@
 /**
- * @fileoverview This is the main entry file to the js used for this project
+ * @fileoverview This is the main entry file for the JavaScript used in this project.
  */
-
 
 document.addEventListener('DOMContentLoaded', () => {
   // Sidebar
-  eventHandler('click', '.menu-btn', () => {
-    toggleSideMenu();
-  });
+  eventHandler('click', '.menu-btn', toggleSideMenu);
 
   // Toasts
-  eventHandler('click', '.toast-close', () => {
-    closeToast();
-  });
+  eventHandler('click', '.toast-close', closeToast);
+
+  // Products menu
+  eventHandler('click', '.filter-btn', toggleProductsMenu);
 });
 
+/**
+ * Event handler function to attach event listeners to elements matching the given selector.
+ * @param {string} type - The type of event (e.g., 'click').
+ * @param {string} selector - The CSS selector for the target elements.
+ * @param {Function} callback - The function to be executed when the event is triggered.
+ * @param {Object} [options] - Optional event listener options.
+ */
 const eventHandler = (type, selector, callback, options) => {
   document.addEventListener(type, (e) => {
     if (e.target.matches(selector)) {
       callback(e);
     }
   }, options);
-}
+};
 
+/**
+ * Toggles the visibility of the sidebar menu.
+ * If the sidebar is currently hidden, it will be shown; otherwise, it will be hidden.
+ */
 const toggleSideMenu = () => {
   const sidebar = document.querySelector('.mobile-nav');
+  if (sidebar) {
+    sidebar.style.width = sidebar.getBoundingClientRect().width === 0 ? '280px' : '0px';
+  } else {
+    console.error('Sidebar element not found');
+  }
+};
 
-    if (sidebar.getBoundingClientRect().width === 0) {
-      sidebar.style.width = '280px';
-    } else {
-      sidebar.style.width = '0px';
-    }
-}
-
+/**
+ * Closes the toast notification by setting its display style to 'none'.
+ */
 const closeToast = () => {
-  document.querySelector('.toast').style.display = 'none';
+  const toast = document.querySelector('.toast');
+  if (toast) {
+    toast.style.display = 'none';
+  } else {
+    console.error('Toast element not found');
+  }
+};
+
+const toggleProductsMenu = () => {
+  const productsMenu = document.querySelector('.products-mobile-menu');
+
+  if (productsMenu.getBoundingClientRect().height == 0) {
+    productsMenu.style.height = '450px';
+  } else {
+    productsMenu.style.height = '0px';
+  }
 }
