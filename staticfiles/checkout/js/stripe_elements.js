89c1a05185971339bd3d3bb5cec21faf9fec3b2d
Craig Gent <genty83@outlook.com>
2024-11-28 13:18:16 +0000

    Collected static

--- staticfiles/checkout/js/stripe_elements.js
+++ staticfiles/checkout/js/stripe_elements.js
@@ -4,28 +4,29 @@ var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
     base: {
-        color: '#000',
+        color: '#f4f6fc',
+        iconColor: '#a8aeb3',
         fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
         fontSmoothing: 'antialiased',
         fontSize: '16px',
         '::placeholder': {
-            color: '#aab7c4'
+            color: '#a8aeb3',
         }
     },
     invalid: {
-        color: '#f87171',
-        iconColor: '#f87171'
+        color: '#dc3545',
+        iconColor: '#dc3545'
     }
 };
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
-// Handle realtime validation errors on the card element
+// Handle real-time validation errors on the card element
 card.addEventListener('change', function (event) {
     var errorDiv = document.getElementById('card-errors');
     if (event.error) {
         var html = `
-            <span class="icon" role="alert">
+            <span role="alert">
                 <i class="fas fa-times"></i>
             </span>
             <span>${event.error.message}</span>
@@ -39,14 +40,16 @@ card.addEventListener('change', function (event) {
 // Handle form submit
 var form = document.getElementById('payment-form');
 
-form.addEventListener('submit', function(ev) {
+form.addEventListener('submit', function (ev) {
     ev.preventDefault();
     card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
     $('#payment-form').fadeToggle(100);
     $('#loading-overlay').fadeToggle(100);
-
-    var saveInfo = Boolean($('#id-save-info').attr('checked'));
+    var saveInfo = false;
+    if (document.getElementById('id-save-info')) {
+        saveInfo = document.getElementById('id-save-info').checked;
+    }
     // From using {% csrf_token %} in the form
     var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
     var postData = {
@@ -54,6 +57,7 @@ form.addEventListener('submit', function(ev) {
         'client_secret': clientSecret,
         'save_info': saveInfo,
     };
+    console.log('Post data:', postData); // Debugging statement
     var url = '/checkout/cache_checkout_data/';
 
     $.post(url, postData).done(function () {
@@ -89,7 +93,7 @@ form.addEventListener('submit', function(ev) {
             if (result.error) {
                 var errorDiv = document.getElementById('card-errors');
                 var html = `
-                    <span class="icon" role="alert">
+                    <span role="alert">
                     <i class="fas fa-times"></i>
                     </span>
                     <span>${result.error.message}</span>`;
@@ -107,5 +111,6 @@ form.addEventListener('submit', function(ev) {
     }).fail(function () {
         // just reload the page, the error will be in django messages
         location.reload();
-    })
+    });
 });
+
