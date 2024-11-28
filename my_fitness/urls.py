f25b16c8d8bd8739d2dca479975d11ceb8dc1240
Craig Gent <genty83@outlook.com>
2024-11-28 13:03:13 +0000

    Updated file

--- my_fitness/urls.py
+++ my_fitness/urls.py
@@ -6,6 +6,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import handler404
+from .views import my_fitness_404_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,4 +23,4 @@ urlpatterns = [
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-handler404 = views.my_fitness_404_view
+handler404 = my_fitness_404_view
